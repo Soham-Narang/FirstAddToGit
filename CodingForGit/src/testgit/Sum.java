36b81da3484ff7557262caf86e899c2fbc051591
Soham Narang <narangsoham5856@gmail.com>
2021-11-19 11:52:46 -0800

    code added to sum class

--- CodingForGit/src/testgit/Sum.java
+++ CodingForGit/src/testgit/Sum.java
@@ -3,8 +3,12 @@ package testgit;
 public class Sum {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int a = 5;
+		int b = 4;
+		
+		System.out.println(a+b);
+		
 	}
 
 }
